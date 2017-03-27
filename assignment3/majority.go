0df6b09bd82ab98944dc56c1ba2356a9ddb71fcd
tokams <t.gliniecki@hotmail.com>
2017-03-27 13:51:24 +0200

    assignment3

--- assignment3/majority.go
+++ assignment3/majority.go
@@ -4,6 +4,28 @@ import (
 	"fmt"
 )
 
+func hasMajorityGreedy(data []int) bool {
+	count := 0
+	n := len(data)
+	for i, _ := range data {
+		//me := data[i]
+		for j, _ := range data {
+			if j < len(data) && i < len(data){
+				if data[i] != data[j] {
+					data = append(data[:j], data[j+1:]...)
+					data = append(data[:j+1], data[j+2:]...)
+					count += j
+					break
+				}
+			}
+		}
+	}
+	if count >= n {
+		return true
+	}
+	return false
+}
+
 func hasMajority(data []int) bool {
 	var me int
 	count := 0
@@ -38,5 +60,9 @@ func main() {
 	fmt.Println(hasMajority(data1))
 	fmt.Println(hasMajority(data2))
 	fmt.Println(hasMajority(data3))
+
+	fmt.Println(hasMajorityGreedy(data1))
+	fmt.Println(hasMajorityGreedy(data2))
+	fmt.Println(hasMajorityGreedy(data3))
 
 }
